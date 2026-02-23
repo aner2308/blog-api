374c47245576933e035709a4b79d409b2d9b3827
Anton <erikssonanton94@gmail.com>
2026-02-23 11:02:14 +0100

    Skapat routes för att skapa, hämta in, uppdatera och radera mina blogginlägg

---
+++ routes/postRoutes.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const Post = require("../models/Post");
+const auth = require("../middleware/authMiddleware");
+
+//Hämta alla inlägg
+router.get("/", async (req, res) => {
+
+    const posts = await Post.find();
+
+    res.json(posts);
+});
+
+//Hämta specifikt inlägg
+router.get("/:id", async (req, res) => {
+
+    const post = await Post.findById(req.params.id);
+
+    res.json(post);
+});
+
+//Skapa nytt inlägg
+router.post("/", auth, async (req, res) => {
+
+    const newPost = new Post(req.body);
+    const savedPost = await newPost.save();
+
+    res.json(savedPost);
+});
+
+//Uppdatera inlägg
+router.put("/:id", auth, async (req, res) => {
+
+    const updated = await Post.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.json(updated);
+});
+
+//Radera inlägg
+router.delete("/:id", auth, async (req, res) => {
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.json("Post deleted");
+});
+
+module.exports = router;
